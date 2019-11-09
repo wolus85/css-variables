54eda4cb28216554f67bae5a228b0e3f4310c534
Paweł Wolski <pawelwolski3@gmail.com>
2019-11-09 13:37:43 +0100

    last changes

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ console.log(inputs)
 const arr = [...inputs]
 // inputs.forEach(input => )
 
-function handleUpdate () {
-  document.documentElement.style.setProperty(`--${this.name}`, this.value)
+function handleUpdate() {
+  console.log(this.name)
+  console.log(this.value)
+  const suffix = this.dataset.sizing || '';
+  document.querySelector('img').style.setProperty(`--${this.name}`, this.value + suffix)
 
 }
 
 //może byc w handleUpdate pez parametru i this.value
 
-arr.forEach(input=> input.addEventListener('change', handleUpdate))
+arr.forEach(input => input.addEventListener('change', handleUpdate))
